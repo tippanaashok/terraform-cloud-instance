8692d8f7f62a747a2c84d91db20834a8f87b271f
tippanaashok <86659726+tippanaashok@users.noreply.github.com>
2021-12-24 16:34:47 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "web" {
   ami                         = "ami-04505e74c0741db8d" #Ubuntu 20.04 LTS Free Tier Image
-  instance_type               = "t2.micro"
+  instance_type               = "t3.micro"
   key_name                    = "ansible-key"
   subnet_id                   = aws_subnet.public_subnet.id
   vpc_security_group_ids      = [aws_security_group.allow_http_ssh.id]
   user_data                   = file("nginx-install.sh")
-  availability_zone           = "us-east-1a"
+  availability_zone           = "us-east-1d"
   associate_public_ip_address = true
 
   tags = {
