381b2e606daf1551ef87416d7e1ccbc4e856db85
tippanaashok <86659726+tippanaashok@users.noreply.github.com>
2021-12-24 16:07:22 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
-  ami                         = "ami-09e67e426f25ce0d7" #Ubuntu 20.04 LTS Free Tier Image
-  instance_type               = "t3.micro"
-  key_name                    = "sudhams_virginia_demo"
+  ami                         = "ami-04505e74c0741db8d" #Ubuntu 20.04 LTS Free Tier Image
+  instance_type               = "t2.micro"
+  key_name                    = "ansible-key"
   subnet_id                   = aws_subnet.public_subnet.id
   vpc_security_group_ids      = [aws_security_group.allow_http_ssh.id]
   user_data                   = file("nginx-install.sh")
